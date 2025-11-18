d44e7ea5b34e5b15316cb7cae0ee06ee49fe974b
solBelen07 <sol0770belen@gmail.com>
2025-11-18 20:26:31 -0300

    agregue la clase y su test

---
+++ SegundoParcialProgramacionBasicaII/test/ar/edu/unlam/pb2/MaestroElementalTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MaestroElementalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
