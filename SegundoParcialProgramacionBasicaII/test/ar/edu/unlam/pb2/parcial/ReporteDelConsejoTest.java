6b9dd85e7aecd29b07ae2af4c7302ff088617de2
solBelen07 <sol0770belen@gmail.com>
2025-11-20 18:12:44 -0300

    se corrige  y pasa a junit 4 los test hechos con junit5

--- SegundoParcialProgramacionBasicaII/test/ar/edu/unlam/pb2/parcial/ReporteDelConsejoTest.java
+++ SegundoParcialProgramacionBasicaII/test/ar/edu/unlam/pb2/parcial/ReporteDelConsejoTest.java
@@ -1,11 +1,14 @@
 package ar.edu.unlam.pb2.parcial;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.jupiter.api.Test;
 
 class ReporteDelConsejoTest {
 
